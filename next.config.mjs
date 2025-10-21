e935d1297c9881d021894544544da448cf13f4e6
@infiniteDev <infinitedev0@gmail.com>
2025-10-21 12:17:57 +0300

    second1

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   i18n: {
     locales: ['eng-ae', 'fra-fr', 'deu-de'], // use real language-region codes
     defaultLocale: 'eng-ae',
-    localeDetection: true, // detects browser language
+    localeDetection: false, // detects browser language
   },
 };
 
---
+++ src/app/(dashboard)/MyVn/layout.js
@@ -0,0 +1,13 @@
+// app/(dashboard)/layout.js
+
+
+
+export default function MarketingLayout({ children }) {
+  return (
+    <>
+    <div>
+        
+    </div>
+    </>
+  );
+}
